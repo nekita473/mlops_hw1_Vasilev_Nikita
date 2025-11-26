bf5eaaf68af6517a886ef2042cac2e814ef9fd02
nekita473 <nekita473@yandex.ru>
2025-11-26 16:50:14 +0000

    Uploaded src and dvc.lock

--- src/prepare.py
+++ src/prepare.py
@@ -2,13 +2,26 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd
 import os
+import yaml
 
 
-data = pd.read_csv('data/raw/data.csv')
-le = LabelEncoder()
-data['Species'] = le.fit_transform(data['Species'].values)
-train_data, val_data = train_test_split(data, test_size=0.2,
-                       stratify=data['Species'], random_state=42)
-os.makedirs('data/processed', exist_ok=True)
-train_data.to_csv('data/processed/train.csv', index=False)
-val_data.to_csv('data/processed/val.csv', index=False)
+def main():
+    data = pd.read_csv('data/raw/data.csv')
+    with open('params.yaml', 'r') as file:
+        loaded_data = yaml.safe_load(file)
+    test_size = loaded_data['test_size']
+    random_state = loaded_data['random_state']
+
+    le = LabelEncoder()
+    data['Species'] = le.fit_transform(data['Species'].values)
+
+    train_data, val_data = train_test_split(data, test_size=test_size,
+                        stratify=data['Species'], random_state=random_state)
+
+    os.makedirs('data/processed', exist_ok=True)
+    train_data.to_csv('data/processed/train.csv', index=False)
+    val_data.to_csv('data/processed/val.csv', index=False)
+
+
+if __name__ == "__main__":
+    main()
--- src/train.py
+++ src/train.py
@@ -3,23 +3,32 @@ from sklearn.metrics import accuracy_score
 import pandas as pd
 import mlflow
 import pickle
-
+import yaml
 mlflow.set_tracking_uri("http://localhost:5000")
 
-with mlflow.start_run():
-    train_data = pd.read_csv('data/processed/train.csv')
-    val_data = pd.read_csv('data/processed/val.csv')
+def main():
+    with mlflow.start_run():
+        train_data = pd.read_csv('data/processed/train.csv')
+        val_data = pd.read_csv('data/processed/val.csv')
+        with open('params.yaml', 'r') as file:
+            loaded_data = yaml.safe_load(file)
+        random_state = loaded_data['random_state']
+        max_iter = loaded_data['max_iter']
 
-    X_train, y_train = train_data.iloc[:, :-1], train_data.iloc[:, -1]
-    X_val, y_val = val_data.iloc[:, :-1], val_data.iloc[:, -1]
+        X_train, y_train = train_data.iloc[:, :-1], train_data.iloc[:, -1]
+        X_val, y_val = val_data.iloc[:, :-1], val_data.iloc[:, -1]
 
-    model = LogisticRegression(random_state=42, max_iter=20)
-    model.fit(X_train, y_train)
-    pickle.dump(model, open('model.pkl', 'wb'))
+        model = LogisticRegression(random_state=random_state, max_iter=max_iter)
+        model.fit(X_train, y_train)
+        pickle.dump(model, open('model.pkl', 'wb'))
 
-    preds = model.predict(X_val)
-    accuracy = accuracy_score(y_val, preds)
+        preds = model.predict(X_val)
+        accuracy = accuracy_score(y_val, preds)
 
-    mlflow.log_param("model", "LogisticRegression")
-    mlflow.log_metric("accuracy", accuracy)
-    mlflow.log_artifact("model.pkl")
+        mlflow.log_param("model", "LogisticRegression")
+        mlflow.log_metric("accuracy", accuracy)
+        mlflow.log_artifact("model.pkl")
+
+
+if __name__ == "__main__":
+    main()
