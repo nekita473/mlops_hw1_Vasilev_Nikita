d3f6e85f8a08a7ca9e5a346969459be7da018778
nekita473 <nekita473@yandex.ru>
2025-11-26 16:04:17 +0000

    Updated

--- src/prepare.py
+++ src/prepare.py
@@ -1,11 +1,14 @@
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd
+import os
+
 
 data = pd.read_csv('data/raw/data.csv')
 le = LabelEncoder()
 data['Species'] = le.fit_transform(data['Species'].values)
 train_data, val_data = train_test_split(data, test_size=0.2,
- stratify=data['Species'], random_state=42)
+                       stratify=data['Species'], random_state=42)
+os.makedirs('data/processed', exist_ok=True)
 train_data.to_csv('data/processed/train.csv', index=False)
 val_data.to_csv('data/processed/val.csv', index=False)
